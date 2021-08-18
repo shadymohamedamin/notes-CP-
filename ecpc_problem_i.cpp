d501595ea2bc5b996b510a7185fa67443f9a4609
shadymohamedamin <60430920+shadymohamedamin@users.noreply.github.com>
2021-08-18 15:54:04 +0200

    Create ecpc_problem_i.cpp

---
+++ ecpc_problem_i.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+ll test;
+cin>>test;
+while(test--)
+{
+    ll n;
+    cin>>n;
+    ll arr[n];
+    for(ll i=0;i<n;i++)
+    cin>>arr[i];
+    ll sum=0;
+    for(ll i=0;i<n;i++)
+    {
+        for(ll j=i;j<n;j++)
+        {
+            ll c=0;
+            for(ll k=i;k<=j;k++)
+            {
+                c+=arr[k];
+                //cout<<arr[k]<<" ";
+            }
+            sum+=c;
+            //cout<<endl;
+        }
+    }
+    //cout<<sum;
+    cout<<1.0*(1.0*sum/((n*(n+1))/2));
+}
+return 0;
+}
